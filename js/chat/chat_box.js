1fcdc62c0f808abd41cc95b94c807cdaf35a96da
Andy0414 <pjh8667@naver.com>
2019-04-24 12:32:22 +0900

    UPDATE resize

--- js/chat/chat_box.js
+++ js/chat/chat_box.js
@@ -5,12 +5,25 @@ class ChatBox{
             this.prop.controller = this
         this.x = 0;
         this.y = 0;
+        this.sizeX = 200;
+        this.sizeY = 300;
     }
     getX(){
         return this.x
     }
     getY(){
         return this.y
+    }
+    getSizeX(){
+        return this.sizeX
+    }
+    getSizeY(){
+        return this.sizeY
+    }
+    setSize(x,y){
+        this.sizeX = x <= 200 ? 200 : x
+        this.sizeY = y <= 300 ? 300 : y
+        this.updateRender()
     }
     setProp(prop){
         this.prop = prop
@@ -24,5 +37,7 @@ class ChatBox{
     updateRender(){
         this.prop.style.top = `${this.y}px`
         this.prop.style.left = `${this.x}px`
+        this.prop.style.width = `${this.sizeX}px`
+        this.prop.style.height = `${this.sizeY}px`
     }
 }
--- js/chat/chat_iframe.js
+++ js/chat/chat_iframe.js
@@ -1,20 +1,36 @@
 var chat = new ChatBox();
 chat.setProp(document.getElementsByClassName('chatBox')[0])
+
 var target = null;
+var resizeTarget = null;
 var currentX = 0;
 var currentY = 0;
+var currentSizeX = 0;
+var currentSizeY = 0;
 document.addEventListener("mousedown", (e) => {
-    if (e.target.parentElement.classList.contains("chatBox")) {
+    if (e.target.classList.contains("chatBox__resize")) {
+        resizeTarget = e.target.parentElement
+        currentSizeX = resizeTarget.controller.getSizeX()
+        currentSizeY = resizeTarget.controller.getSizeY()
+    }
+    else if (e.target.parentElement.classList.contains("chatBox")) {
         target = e.target.parentElement
         currentX = e.clientX - target.controller.getX()
         currentY = e.clientY - target.controller.getY()
     }
+    
 })
 document.addEventListener("mousemove", (e) => {
-    console.log("H")
     if (target)
-        target.controller.setPos(e.clientX - currentX,e.clientY-currentY)
+        target.controller.setPos(e.clientX - currentX, e.clientY - currentY)
+    if(resizeTarget)
+    {
+        resizeTarget.controller.setSize(
+            currentSizeX + (e.clientX - currentSizeX - resizeTarget.controller.getX()),
+            currentSizeY + (e.clientY - currentSizeY - resizeTarget.controller.getY()))
+    }
 })
 document.addEventListener("mouseup", (e) => {
     target = null
+    resizeTarget = null
 })

fff81440ddc20ccf309c7fc75e12c7a1029b9371
Andy0414 <pjh8667@naver.com>
2019-04-25 13:51:21 +0900

    UPDATE animation

--- js/chat/chat_iframe.js
+++ js/chat/chat_iframe.js
@@ -3,34 +3,63 @@ chat.setProp(document.getElementsByClassName('chatBox')[0])
 
 var target = null;
 var resizeTarget = null;
+var isClick = false;
 var currentX = 0;
 var currentY = 0;
+var currentVelocityX = 0;
+var currentVelocityY = 0;
 var currentSizeX = 0;
 var currentSizeY = 0;
+var loop = null;
 document.addEventListener("mousedown", (e) => {
+    isClick = true
     if (e.target.classList.contains("chatBox__resize")) {
         resizeTarget = e.target.parentElement
         currentSizeX = resizeTarget.controller.getSizeX()
         currentSizeY = resizeTarget.controller.getSizeY()
-    }
-    else if (e.target.parentElement.classList.contains("chatBox")) {
+    } else if (e.target.parentElement.classList.contains("chatBox")) {
         target = e.target.parentElement
         currentX = e.clientX - target.controller.getX()
         currentY = e.clientY - target.controller.getY()
+        if (loop) clearInterval(loop)
+        loop = setInterval(() => {
+            if (Math.abs(currentVelocityX) >= 0.0001 || Math.abs(currentVelocityY) >= 0.0001) {
+                var changeX = (target.controller.getX() + currentVelocityX)
+                var changeY = (target.controller.getY() + currentVelocityY)
+
+                if (changeX + target.controller.getSizeX() < innerWidth) {
+                    currentVelocityX /= 1.1
+                } else {
+                    currentVelocityX -= ((innerWidth - target.controller.getSizeX()) - currentSizeX) / 1000
+                }
+
+                if (changeY + target.controller.getSizeY() < innerHeight) {
+                    currentVelocityY /= 1.1
+                } else {
+                    currentVelocityY -= ((innerHeight - target.controller.getSizeY()) - currentSizeY) / 1000
+                }
+
+                target.controller.setPos(changeX, changeY)
+            } else {
+                target = null
+                clearInterval(this)
+            }
+        }, 1000 / 60)
     }
-    
 })
 document.addEventListener("mousemove", (e) => {
-    if (target)
-        target.controller.setPos(e.clientX - currentX, e.clientY - currentY)
-    if(resizeTarget)
-    {
+    if (target && isClick) {
+        // target.controller.setPos(e.clientX - currentX, e.clientY - currentY)
+        currentVelocityX = (e.clientX - (target.controller.getX() + currentX)) / 5
+        currentVelocityY = (e.clientY - (target.controller.getY() + currentY)) / 5
+    }
+    if (resizeTarget) {
         resizeTarget.controller.setSize(
             currentSizeX + (e.clientX - currentSizeX - resizeTarget.controller.getX()),
             currentSizeY + (e.clientY - currentSizeY - resizeTarget.controller.getY()))
     }
 })
 document.addEventListener("mouseup", (e) => {
-    target = null
+    isClick = false;
     resizeTarget = null
 })

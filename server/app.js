b15dc45f4815d1d8746744ac600c5f6be60fe63a
Andy0414 <pjh8667@naver.com>
2019-04-29 21:31:17 +0900

    UPDATE server

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log('connect')
+    socket.on('sendMsg', data => {
+        console.log(data)
+        io.emit('getMsg', data)
+    })
+})
+
+http.listen(3000, () => {
+    console.log("server open");
+})
